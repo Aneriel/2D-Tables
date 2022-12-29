0102ceeab6dcaf27e3aa80797db11c65eae6058c
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-12-29 12:50:58 +0100

    Ready

--- 2D-Tables/Program.cs
+++ 2D-Tables/Program.cs
@@ -6,26 +6,70 @@ namespace _2D_Tables
     {
         static void Main(string[] args)
         {
-            string[] size =Console.ReadLine().Split(" ");
-            int wiersze = int.Parse(size[0]);
-            int kolumny = int.Parse(size[1]);
-            int[,] macierz = new int[wiersze,kolumny];
-            for(int i = 0; i < wiersze; i++)
+            string[] inputA = Console.ReadLine().Split(' ');
+            int n1 = int.Parse(inputA[0]);
+            int m1 = int.Parse(inputA[1]);
+            int[,] matrixA = new int[n1, m1];
+            string[] rowValuesA = Console.ReadLine().Split(' ');
+            int rowIndexA = 0;
+            int colIndexA = 0;
+            foreach (string value in rowValuesA)
             {
-                string[] rowNums = Console.ReadLine().Split(" ");
-                for (int j=0; j< kolumny; j++)
+                matrixA[rowIndexA, colIndexA] = int.Parse(value);
+                colIndexA++;
+                if (colIndexA == m1)
                 {
-                    macierz[i, j] = int.Parse(rowNums[j]);
+                    rowIndexA++;
+                    colIndexA = 0;
                 }
             }
-            for(int i = 0; i<kolumny; i++)
+            string[] inputB = Console.ReadLine().Split(' ');
+            int n2 = int.Parse(inputB[0]);
+            int m2 = int.Parse(inputB[1]);
+            if (m1 != n2)
             {
-                for(int j=0; j<wiersze; j++)
-                {
-                    Console.Write("{0} ", macierz[j,i]);
-                }
-                Console.WriteLine();
+                Console.WriteLine("impossible");
             }
+            else
+            {
+                int[,] matrixB = new int[n2, m2];
+                string[] rowValuesB = Console.ReadLine().Split(' ');
+                int rowIndexB = 0;
+                int colIndexB = 0;
+                foreach (string value in rowValuesB)
+                {
+                    matrixB[rowIndexB, colIndexB] = int.Parse(value);
+                    colIndexB++;
+                    if (colIndexB == m2)
+                    {
+                        rowIndexB++;
+                        colIndexB = 0;
+                    }
+                }
+                int[,] resultMatrix = new int[n1, m2];
+                for (int i = 0; i < n1; i++)
+                {
+                    for (int j = 0; j < m2; j++)
+                    {
+                        int result = 0;
+                        for (int k = 0; k < m1; k++)
+                        {
+                            result += matrixA[i, k] * matrixB[k, j];
+                        }
+                        resultMatrix[i, j] = result;
+                    }
+                }
+                for (int i = 0; i < n1; i++)
+                {
+                    for (int j = 0; j < m2; j++)
+                    {
+                        Console.Write(resultMatrix[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
