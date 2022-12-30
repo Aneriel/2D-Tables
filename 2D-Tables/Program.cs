52423d5cc7b739cccea5ac81812eb46936a1aca5
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-12-30 19:24:12 +0100

    gotowe

--- 2D-Tables/Program.cs
+++ 2D-Tables/Program.cs
@@ -1,75 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _2D_Tables
+namespace ColumnWithLargestSum
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            string[] inputA = Console.ReadLine().Split(' ');
-            int n1 = int.Parse(inputA[0]);
-            int m1 = int.Parse(inputA[1]);
-            int[,] matrixA = new int[n1, m1];
-            string[] rowValuesA = Console.ReadLine().Split(' ');
-            int rowIndexA = 0;
-            int colIndexA = 0;
-            foreach (string value in rowValuesA)
+            List<int[]> data = new List<int[]>();
+            while (true)
             {
-                matrixA[rowIndexA, colIndexA] = int.Parse(value);
-                colIndexA++;
-                if (colIndexA == m1)
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
                 {
-                    rowIndexA++;
-                    colIndexA = 0;
+                    break;
                 }
+                int[] row = Array.ConvertAll(line.Split(' '), int.Parse);
+                data.Add(row);
             }
-            string[] inputB = Console.ReadLine().Split(' ');
-            int n2 = int.Parse(inputB[0]);
-            int m2 = int.Parse(inputB[1]);
-            if (m1 != n2)
+            int[] columnSums = new int[data[0].Length];
+            for (int i = 0; i < data.Count; i++)
             {
-                Console.WriteLine("impossible");
-            }
-            else
-            {
-                int[,] matrixB = new int[n2, m2];
-                string[] rowValuesB = Console.ReadLine().Split(' ');
-                int rowIndexB = 0;
-                int colIndexB = 0;
-                foreach (string value in rowValuesB)
+                for (int j = 0; j < data[i].Length; j++)
                 {
-                    matrixB[rowIndexB, colIndexB] = int.Parse(value);
-                    colIndexB++;
-                    if (colIndexB == m2)
-                    {
-                        rowIndexB++;
-                        colIndexB = 0;
-                    }
-                }
-                int[,] resultMatrix = new int[n1, m2];
-                for (int i = 0; i < n1; i++)
-                {
-                    for (int j = 0; j < m2; j++)
-                    {
-                        int result = 0;
-                        for (int k = 0; k < m1; k++)
-                        {
-                            result += matrixA[i, k] * matrixB[k, j];
-                        }
-                        resultMatrix[i, j] = result;
-                    }
-                }
-                for (int i = 0; i < n1; i++)
-                {
-                    for (int j = 0; j < m2; j++)
-                    {
-                        Console.Write(resultMatrix[i, j] + " ");
-                    }
-                    Console.WriteLine();
+                    columnSums[j] += data[i][j];
                 }
             }
 
-            Console.ReadKey();
+            int maxSum = 0;
+            int maxSumIndex = 0;
+            for (int i = 0; i < columnSums.Length; i++)
+            {
+                if (columnSums[i] > maxSum)
+                {
+                    maxSum = columnSums[i];
+                    maxSumIndex = i;
+                }
+            }
+
+            Console.WriteLine(maxSumIndex);
         }
     }
 }
